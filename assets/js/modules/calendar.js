ec46aa346639ac2f6e88f0eeb6bcdab53eee463f
comm-hub-myco <comm.hub.myco@gmail.com>
2025-11-15 21:02:25 -0500

    Update calendar.js
    disabling debug

--- assets/js/modules/calendar.js
+++ assets/js/modules/calendar.js
@@ -163,7 +163,7 @@ const Calendar = (() => {
       const host = document.querySelector(selector);
       if (!host) return;
       const events = await loadEvents();
-      document.querySelector(selector).insertAdjacentHTML('beforebegin', `<pre style="white-space:pre-wrap;opacity:.7">[debug] ${JSON.stringify(events, null, 2)}</pre>`);
+      //document.querySelector(selector).insertAdjacentHTML('beforebegin', `<pre style="white-space:pre-wrap;opacity:.7">[debug] ${JSON.stringify(events, null, 2)}</pre>`);
       const view = luxon.DateTime.local();
       console.info('[Calendar] Loaded events:', events);
       renderCalendar(host, view, events);
